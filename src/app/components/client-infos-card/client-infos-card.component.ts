418514ef5fcf5ca2712256c4a327b530a50acc47
thiago_osim <thiago_osim@hotmail.com>
2026-02-12 19:39:33 -0300

    Criado o componente client-infos-card

---
+++ src/app/components/client-infos-card/client-infos-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-infos-card',
+  templateUrl: './client-infos-card.component.html',
+  styleUrls: ['./client-infos-card.component.scss']
+})
+export class ClientInfosCardComponent {
+
+}
