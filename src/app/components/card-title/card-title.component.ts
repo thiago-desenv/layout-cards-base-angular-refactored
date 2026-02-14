6118730a8ebce5770ea29eb6ab932f191dc2e21f
thiago_osim <thiago_osim@hotmail.com>
2026-02-14 16:51:10 -0300

    Aplicado card-title para balance-card

--- src/app/components/card-title/card-title.component.ts
+++ src/app/components/card-title/card-title.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-title',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-title.component.scss']
 })
 export class CardTitleComponent {
-
+  @Input({required: true}) title: string = '';
+  @Input({required: true}) icon: string = '';
 }
