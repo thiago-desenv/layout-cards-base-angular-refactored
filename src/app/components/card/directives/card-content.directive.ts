d2a3112d0169b5e10345484fb8aeb8683d8bee10
thiago_osim <thiago_osim@hotmail.com>
2026-03-09 23:09:30 -0300

    Aplicado projeção de conteúdo no balance-card

--- src/app/components/card/directives/card-content.directive.ts
+++ src/app/components/card/directives/card-content.directive.ts
@@ -2,6 +2,6 @@ import { Directive } from '@angular/core';
 
 @Directive({
   selector: 'app-card-content',
-  host: { 'class': 'ca-c-card_content' }
+  host: { 'class': 'ca-c-card__content' }
 })
 export class CardContentDirective {}
--- src/app/components/card/directives/card-header.directive.ts
+++ src/app/components/card/directives/card-header.directive.ts
@@ -2,6 +2,6 @@ import { Directive } from '@angular/core';
 
 @Directive({
   selector: 'app-card-header',
-  host: { 'class': 'ca-c-card_header' }
+  host: { 'class': 'ca-c-card__header' }
 })
 export class CardHeaderDirective {}
