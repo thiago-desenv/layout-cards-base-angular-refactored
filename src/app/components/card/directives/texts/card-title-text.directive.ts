c9efd04bad0e64f7e721e58ea086533efa1f3e7c
thiago_osim <thiago_osim@hotmail.com>
2026-03-10 22:31:03 -0300

    Movido as diretivas para a pasta base e criado a diretivas card-title-text e adicionado na pasta texts

---
+++ src/app/components/card/directives/texts/card-title-text.directive.ts
@@ -0,0 +1,7 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCardTitleText]',
+  host: { 'class': 'ca-c-card__title-text' }
+})
+export class CardTitleTextDirective {}
